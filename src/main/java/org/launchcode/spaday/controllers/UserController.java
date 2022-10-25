6eeb43d86747a932169718c389297cdd1afc76c1
Emily Spradling <spradling.ed@gmail.com>
2022-10-24 20:04:44 -0500

    still working

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -1,9 +1,17 @@
 package org.launchcode.spaday.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("User")
+@RequestMapping("user")
 public class UserController {
+
+    @RequestMapping("add")
+    public String displayAddUserForm(){
+        return "add";
+    }
+
 }
+
